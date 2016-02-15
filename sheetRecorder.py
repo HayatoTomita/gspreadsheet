2c4e4fbefadaae5d3fd3465790846313f50ed06d
HayatoTomita <tomita@team-lab.com>
2016-02-15 17:45:20 +0900

    Introduce docopt

--- sheetRecorder.py
+++ sheetRecorder.py
@@ -1,4 +1,17 @@
 # -*- coding:utf-8 -*-
+"""sheetRecorder
+
+Record some values to GoogleSpreadSheet
+Usage:
+  sheetRecorder.py <sheet_number> <cell_number> <value> [options]
+
+Options:
+  -h --help                Show this screen.
+  --version                Show version.
+"""
+from docopt import docopt
+
+
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 import settings
@@ -9,15 +22,15 @@ CREDENTIALS = SignedJwtAssertionCredentials(
                  settings.SCOPE)
 
 
-def main():
+def update():
     gc = gspread.authorize(CREDENTIALS)
     gfile = gc.open_by_key(settings.DOC_ID)
-    wsheet = gfile.get_worksheet(1)
-    wsheet.update_acell('C3', 'hoge')
-    records = wsheet.get_all_records()
+    wsheet = gfile.get_worksheet(int(args['<sheet_number>']))
+    wsheet.update_acell(args['<cell_number>'], args['<value>'])
 
     for record in records:
         print(record)
 
 if __name__ == '__main__':
-    main()
+    args = docopt(__doc__, version="0.0.1")
+    update()
